536663208f16383ba8284ee7a8a14f436ca25e46
Muhammad Saad Hasan <saadhassan817@gmail.com>
2024-05-25 13:22:46 +0500

    admin panel

--- app/page.js
+++ app/page.js
@@ -1,6 +1,16 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import Image from "next/image";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/admin");
+  }, []);
+
   return (
     <main className="flex flex-col items-center justify-start w-full h-auto p-4"></main>
   );
